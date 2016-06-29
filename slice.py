435823154fcbd38bd108279177301317542854ce
JaceyKan <jacey_kan@sina.com>
2016-06-29 22:55:11 +0800

    learn slice

---
+++ slice.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+L=['Apple','Banana','Coconut','Orange','blueberry']
+
+r=[]
+n=5
+for i in range(n):
+    r.append(L[i])
+
+print r
+
+print 'L[0:3]',L[0:3]
+
+print 'L[:3]',L[:3]
+
+print 'L[1:3]',L[1:3]
+
+print 'L[-2:]',L[-2:]
+
+print 'L[-2:-1]',L[-2:-1]
---
+++ slice_number.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+L=range(100)
+print L
+
+print 'L[:10]',L[:10]
+
+print 'L[-10:]',L[-10:]
+
+print 'L[10:20]',L[10:20]
+
+print 'L[:10:2]',L[:10:2]
+
+print 'L[::5]',L[::5]
+
+
